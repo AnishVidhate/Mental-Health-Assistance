51dbe0407e0f3ab00d6c7454cc2b23756ef3c8b4
AnishVidhate <anishvidhate1234@gmail.com>
2025-07-14 00:47:28 +0530

    changes-as-per-connection-string-changed

--- mental-health-assistance/src/Config/Config.js
+++ mental-health-assistance/src/Config/Config.js
@@ -1,6 +1,7 @@
 const config = {
 RAZORPAY_KEY_ID: "rzp_test_rZfdStCvfRhsxH",
-BaseURL:"https://localhost:44364/api"
+// BaseURL:"https://localhost:44364/api"
+BaseURL:"https://mentalhealthassistplatform.onrender.com/api"
 //BaseURL: "http://database-1.c5ok8ywse21w.ap-south-1.rds.amazonaws.com:1433/api"
  //BaseURL:"http://ec2-16-170-254-100.eu-north-1.compute.amazonaws.com:5000/api"
 };
